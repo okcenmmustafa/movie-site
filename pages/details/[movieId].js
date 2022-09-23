a690edf8d49b2f39db384d74f53ddd87dc800c96
okcenmmustafa <mustafaokcen@gmail.com>
2022-09-24 01:26:59 +0200

    add : movie detal page in pages

---
+++ pages/details/[movieId].js
@@ -0,0 +1,49 @@
+import Details from "features/details";
+import request from "utilities/request";
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+export async function getStaticProps({ params }) {
+  try {
+    const movieResponse = await request.get(`movie/${params.movieId}`, {
+      params: {
+        api_key: "d1b38123d56e970dcbee0d95330845fa",
+      },
+    });
+    const similarResponse = await request.get(
+      `movie/${params.movieId}/similar`,
+      {
+        params: {
+          api_key: "d1b38123d56e970dcbee0d95330845fa",
+        },
+      }
+    );
+    const [movie, similarMovies] = [movieResponse.data, similarResponse.data];
+    return {
+      props: {
+        movie: movie,
+        similarMovies: similarMovies,
+      },
+      revalidate: 60,
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      notFound: true,
+      revalidate: 5,
+    };
+  }
+}
+
+export default function HomePageNext({ movie, similarMovies }) {
+  return (
+    <div>
+      <Details movie={movie} similarMovies={similarMovies} />
+    </div>
+  );
+}
