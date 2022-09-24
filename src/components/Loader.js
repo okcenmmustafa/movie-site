a6f5124895a305381eced5fe7bc6ef94c19585fb
okcenmmustafa <mustafaokcen@gmail.com>
2022-09-24 02:22:27 +0200

    add : loader gif

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,9 @@
 import styles from "./Loader.module.css";
-
+import loader from "images/orange_circles.gif"
 export default function Loader() {
   return (
     <div className={`${styles.loader}`} r>
-      Loading
+      <img src={loader.src} width="70px" height="70px"/>
     </div>
   );
 }
